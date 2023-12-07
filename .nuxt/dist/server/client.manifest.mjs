5eda76de0caa520d5882958a1b2c91ba3c0d93b3
kennyxy <kennyxy.sh@gmail.com>
2023-12-07 17:06:08 +0100

    removed builds from gitignore to try and deploy

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///F:/WebDevProjects/AliexpressClone/aliexpress-clone/aliexpressclone/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///F:/WebDevProjects/AliexpressClone/aliexpress-clone/aliexpressclone/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
